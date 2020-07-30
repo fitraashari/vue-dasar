e990705a4da181234c610f27829db4c15967ab62
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-07-30 18:37:04 +0700

    methods properties

--- asset/js/app.js
+++ asset/js/app.js
@@ -6,10 +6,17 @@ var app = new Vue({
       message2: 'Helo Anjay!',
       int1: 1,
       int2: 2,
+      result:null,
+
     },
     computed: {
         sum: function(){
           return this.int1 + this.int2;
         }
+    },
+    methods: {
+      sumProcess: function(int3){
+        return this.result = this.int1 + this.int2 + int3;
+      }
     }
   })
