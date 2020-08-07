a4f595915c844ef9c31839e13f6ee0a4b3df4575
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-08-07 21:51:00 +0700

    menggunakan properti di dalam component

--- asset/js/app.js
+++ asset/js/app.js
@@ -7,7 +7,14 @@ Vue.component('greeting',{
       </div>
   `
 });
-
+Vue.component('button-counter',{
+  data: function(){
+    return{
+      count:0
+    }
+  },
+  template: '<button v-on:click="count++">Click Me {{count}} times</button>'
+})
 var app = new Vue({
     el: '#app',
   })
